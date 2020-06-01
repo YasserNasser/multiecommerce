373d598a5977973f774f800e0f50db01a61b5dae
Yaser Nasser <yassernasser882@gmail.com>
2020-06-02 00:44:46 +0300

    prepear the admin and the front layouts

---
+++ resources/views/frontend/slider.blade.php
@@ -0,0 +1,161 @@
+{{--<div id="nov-slider" class="slider-wrapper theme-default col-xl-9 col-lg-9 col-md-9 col-md-12"--}}
+     {{--data-effect="random"--}}
+     {{--data-slices="15"--}}
+     {{--data-animSpeed="500"--}}
+     {{--data-pauseTime="10000"--}}
+     {{--data-startSlide="0"--}}
+     {{--data-directionnav="false"--}}
+     {{--data-controlNav="true"--}}
+     {{--data-controlNavThumbs="false"--}}
+     {{--data-pauseOnHover="true"--}}
+     {{--data-manualAdvance="false"--}}
+     {{--data-randomStart="false">--}}
+    {{--<div class="nov_preload">--}}
+        {{--<div class="process-loading active">--}}
+            {{--<div class="loader">--}}
+                {{--<div class="dot"></div>--}}
+                {{--<div class="dot"></div>--}}
+                {{--<div class="dot"></div>--}}
+                {{--<div class="dot"></div>--}}
+                {{--<div class="dot"></div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="nivoSlider">--}}
+        {{--<a href="#">--}}
+            {{--<img src="http://demo.bestprestashoptheme.com/savemart/modules/novnivoslider/images/266cf50ba4d1d91fa5f5ded20bb66ea38de3b350_1.jpg" alt="" title="#htmlcaption_42" />--}}
+        {{--</a>--}}
+        {{--<a href="#">--}}
+            {{--<img src="http://demo.bestprestashoptheme.com/savemart/modules/novnivoslider/images/62896aebffd6fdce749d957fc76bd83d734fa338_2.jpg" alt="" title="#htmlcaption_43" />--}}
+        {{--</a>--}}
+        {{--<a href="#">--}}
+            {{--<img src="http://demo.bestprestashoptheme.com/savemart/modules/novnivoslider/images/195d62088850e3489886855b4239edcc4fb1868f_3.jpg" alt="" title="#htmlcaption_57" />--}}
+        {{--</a>--}}
+    {{--</div>--}}
+    {{--<div id="htmlcaption_42" class="nivo-html-caption">--}}
+        {{--<div class="nov-slider-ct">--}}
+            {{--<div class="nov-center slider-none">--}}
+                {{--<div class="nov-title effect-0" >--}}
+                    {{--Slide Home 3 01--}}
+                {{--</div>--}}
+                {{--<div class="nov-description effect-0" >--}}
+                    {{--<p>Slide Home 3 01</p>--}}
+                {{--</div>--}}
+                {{--<div class="nov-html effect-0">--}}
+                    {{--<p>Slide Home 3 01</p>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--<div id="htmlcaption_43" class="nivo-html-caption">--}}
+        {{--<div class="nov-slider-ct">--}}
+            {{--<div class="nov-center slider-none">--}}
+                {{--<div class="nov-title effect-0" >--}}
+                    {{--Slide Home 3 02--}}
+                {{--</div>--}}
+                {{--<div class="nov-description effect-0" >--}}
+                    {{--<p>Slide Home 3 02</p>--}}
+                {{--</div>--}}
+                {{--<div class="nov-html effect-0">--}}
+                    {{--<p>Slide Home 3 02</p>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--<div id="htmlcaption_57" class="nivo-html-caption">--}}
+        {{--<div class="nov-slider-ct">--}}
+            {{--<div class="nov-center slider-none">--}}
+                {{--<div class="nov-title effect-0" >--}}
+                    {{--Slider Home 3 03--}}
+                {{--</div>--}}
+                {{--<div class="nov-description effect-0" >--}}
+                    {{--<p>Slider Home 3 03</p>--}}
+                {{--</div>--}}
+                {{--<div class="nov-html effect-0">--}}
+                    {{--<p>Slider Home 3 03</p>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+<!-- begin /var/www/demo.bestprestashoptheme.com/public_html/savemart/themes/vinova_savemart/modules/novpagemanage/views/source/slider.tpl -->
+<div id="nov-slider" class="slider-wrapper theme-default col-xl-12 col-lg-12 col-md-12"
+     data-effect="random"
+     data-slices="15"
+     data-animSpeed="500"
+     data-pauseTime="10000"
+     data-startSlide="0"
+     data-directionnav="false"
+     data-controlNav="true"
+     data-controlNavThumbs="false"
+     data-pauseOnHover="true"
+     data-manualAdvance="false"
+     data-randomStart="false">
+    <div class="nov_preload">
+        <div class="process-loading active">
+            <div class="loader">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </div>
+        </div>
+    </div>
+    <div class="nivoSlider">
+        <a href="#">
+            <img src="http://demo.bestprestashoptheme.com/savemart/modules/novnivoslider/images/fd4d1071197697047b743d95d7eb1d4e12dbc789_1.jpg" alt="" title="#htmlcaption_49" />
+        </a>
+        <a href="#">
+            <img src="http://demo.bestprestashoptheme.com/savemart/modules/novnivoslider/images/b8945ba6615c071b3023f0db7111ef0f44ccd05a_2.jpg" alt="" title="#htmlcaption_50" />
+        </a>
+        <a href="#">
+            <img src="http://demo.bestprestashoptheme.com/savemart/modules/novnivoslider/images/5aa54427945d748797165674e281af915b83e9bf_3.jpg" alt="" title="#htmlcaption_51" />
+        </a>
+    </div>
+    <div id="htmlcaption_49" class="nivo-html-caption">
+        <div class="nov-slider-ct">
+            <div class="nov-center slider-none">
+                <div class="nov-title effect-0" >
+                    Slider Home1 01
+                </div>
+                <div class="nov-description effect-0" >
+                    <p>Slider Home1 01</p>
+                </div>
+                <div class="nov-html effect-0">
+                    <p>Slider Home1 01</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="htmlcaption_50" class="nivo-html-caption">
+        <div class="nov-slider-ct">
+            <div class="nov-center slider-none">
+                <div class="nov-title effect-0" >
+                    Slider Home1 02
+                </div>
+                <div class="nov-description effect-0" >
+                    <p>Slider Home1 02</p>
+                </div>
+                <div class="nov-html effect-0">
+                    <p>Slider Home1 02</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="htmlcaption_51" class="nivo-html-caption">
+        <div class="nov-slider-ct">
+            <div class="nov-center slider-none">
+                <div class="nov-title effect-0" >
+                    Slider Home1 03
+                </div>
+                <div class="nov-description effect-0" >
+                    <p>Slider Home1 03</p>
+                </div>
+                <div class="nov-html effect-0">
+                    <p>Slider Home1 03</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/admin', function () {
+//    return view('layouts.admin');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.home');
 });
 
 Auth::routes();
